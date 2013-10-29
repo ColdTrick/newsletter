7b4c189667242e18a2122f20d749bd7b290adc21
Jerome Bakker <jeabakker@coldtrick.com>
2013-10-29 14:05:08 +0100

    added cron hook for sending the newsletters

---
+++ lib/hooks.php
@@ -0,0 +1,13 @@
+<?php
+
+	/**
+	 * The cron hook will take care of sending all the scheduled newsletters
+	 *
+	 * @param string $hook
+	 * @param string $type
+	 * @param string $returnvalue
+	 * @param array $params
+	 */
+	function newsletter_cron_handler($hook, $type, $returnvalue, $params) {
+		
+	}
--- start.php
+++ start.php
@@ -2,13 +2,16 @@
 
 	// load library files
 	require_once(dirname(__FILE__) . "/lib/functions.php");
+	require_once(dirname(__FILE__) . "/lib/hooks.php");
 	
 	// register default Elgg events
 	elgg_register_event_handler("init", "system", "newsletter_init");
 	
 	/**
-	 * This function is calle when the Elgg system gets initialized
+	 * This function is called when the Elgg system gets initialized
 	 */
 	function newsletter_init() {
 		
+		// register plugin hooks
+		elgg_register_plugin_hook_handler("cron", "hourly", "newsletter_cron_handler");
 	}
