7eed87732e6aa8cf3f4c535214a27ce21a55cee5
Jeroen Dalsem <jdalsem@coldtrick.com>
2013-12-20 10:40:48 +0100

    added blog embed navigation

--- views/default/newsletter/embed.php
+++ views/default/newsletter/embed.php
@@ -1,34 +1,51 @@
 <?php
 
 $entity = $vars["entity"];
+$offset = (int) max(get_input("offset", 0), 0);
+$limit = 10;
 
 $options = array(
 		"type" => "object",
 		"subtype" => "blog",
-		"full_view" => false
+		"full_view" => false,
+		"limit" => $limit,
+		"offset" => $offset,
+		"count" => true
 	);
 
 if ($entity->getContainerEntity() instanceof ElggGroup) {
 	$options["container_guid"] = $entity->getContainerGUID();
 }
 
-$entities = elgg_get_entities($options);
+$count = elgg_get_entities($options);
 
-echo "<ul id='newsletter-embed-list'>";
-foreach($entities as $entity) {
-	$description = $entity->description;
+unset($options["count"]);
+
+if ($count > 0) {
+	$entities = elgg_get_entities($options);
+
+	echo "<ul id='newsletter-embed-list'>";
+	foreach($entities as $entity) {
+		$description = $entity->description;
+		
+		echo "<li>";
+		echo "<div>";
+		echo "<strong>" . $entity->title . "</strong> ";
+		echo elgg_get_excerpt($description);
+		echo "</div>";
+		echo "<div class='newsletter-embed-item-content'>";
+		
+		echo "<strong>" . $entity->title . "</strong><br />";
+		echo elgg_view("output/longtext", array("value" => $description));
+		
+		echo "</div></li>";
+	}
 	
-	echo "<li>";
-	echo "<div>";
-	echo "<strong>" . $entity->title . "</strong><br />";
-	echo elgg_get_excerpt($description);
+	echo "</ul>";
+	
+	echo "<div id='newsletter-embed-pagination'>";
+	echo elgg_view("navigation/pagination", array("offset" => $offset, "limit" => $limit, "count" => $count));
 	echo "</div>";
-	echo "<div class='newsletter-embed-item-content'>";
-	
-	echo "<strong>" . $entity->title . "</strong><br />";
-	echo elgg_view("output/longtext", array("value" => $description));
-	
-	echo "</div></li>";
+} else {
+	echo elgg_echo("notfound");
 }
-
-echo "</ul>";
