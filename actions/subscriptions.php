578efdc356cb10c830e8db563a71d046cc7b877b
Jerome Bakker <jeabakker@coldtrick.com>
2013-12-20 09:49:54 +0100

    added: global block on subscriptions page

--- actions/subscriptions.php
+++ actions/subscriptions.php
@@ -1,18 +1,25 @@
 <?php
+/**
+ * Action file to save the subscriptions of a user
+ *
+ */
 
-	/**
-	 * Action file to save the subscriptions of a user
-	 *
-	 */
+$user_guid = (int) get_input("user_guid");
+$subscriptions = get_input("subscriptions");
+$block_all = (int) get_input("block_all");
 
-	$user_guid = (int) get_input("user_guid");
-	$subscriptions = get_input("subscriptions");
+if (!empty($user_guid) && !empty($subscriptions) && is_array($subscriptions)) {
+	$user = get_user($user_guid);
 	
-	if (!empty($user_guid) && !empty($subscriptions) && is_array($subscriptions)) {
-		$user = get_user($user_guid);
+	if (!empty($user) && $user->canEdit()) {
+		$result = true;
 		
-		if (!empty($user) && $user->canEdit()) {
-			$result = true;
+		if (!empty($block_all)) {
+			// block all newsletters
+			$result = newsletter_unsubscribe_all_user($user);
+		} else {
+			// remove block all
+			remove_entity_relationship($user->getGUID(), NewsletterSubscription::GENERAL_BLACKLIST, elgg_get_site_entity()->getGUID());
 			
 			// go through all the subscriptions
 			foreach ($subscriptions as $guid => $value) {
@@ -30,17 +37,18 @@
 					}
 				}
 			}
-			
-			if ($result) {
-				system_message(elgg_echo("newsletter:action:subscriptions:success"));
-			} else {
-				register_error(elgg_echo("newsletter:action:subscriptions:error"));
-			}
+		}
+		
+		if ($result) {
+			system_message(elgg_echo("newsletter:action:subscriptions:success"));
 		} else {
-			register_error(elgg_echo("InvalidParameterException:NoEntityFound"));
+			register_error(elgg_echo("newsletter:action:subscriptions:error"));
 		}
 	} else {
-		register_error(elgg_echo("InvalidParameterException:MissingParameter"));
+		register_error(elgg_echo("InvalidParameterException:NoEntityFound"));
 	}
-	
-	forward(REFERER);
+} else {
+	register_error(elgg_echo("InvalidParameterException:MissingParameter"));
+}
+
+forward(REFERER);
