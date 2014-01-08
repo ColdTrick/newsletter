b5cd9ed6db65d88084e7adf91c826223db4049bc
Jerome Bakker <jeabakker@coldtrick.com>
2014-01-08 14:14:12 +0100

    fixed: copy template to custom from theme/plugin works even if no CSS or HTML provided

--- actions/edit/template_to_custom.php
+++ actions/edit/template_to_custom.php
@@ -20,8 +20,19 @@ if (!empty($guid) && !empty($template)) {
 				}
 			} else {
 				// assume the template is provided in a view
-				$entity->css = elgg_view("newsletter/templates/" . $template . "/css");
-				$entity->html = elgg_view("newsletter/templates/" . $template . "/body");
+				if (elgg_view_exists("newsletter/templates/" . $template . "/css")) {
+					$entity->css = elgg_view("newsletter/templates/" . $template . "/css");
+				} else {
+					// fallback to default CSS content
+					$entity->css = elgg_view("newsletter/templates/default/css");
+				}
+				
+				if (elgg_view_exists("newsletter/templates/" . $template . "/body")) {
+					$entity->html = elgg_view("newsletter/templates/" . $template . "/body");
+				} else {
+					// fallback to default HTML content
+					$entity->html = elgg_view("newsletter/templates/default/body");
+				}
 			}
 
 			system_message(elgg_echo("newsletter:action:template_to_custom:success"));
