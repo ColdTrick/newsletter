e597ab2ebcafbcdf46f1f810c14a427c3485cab7
Jerome Bakker <jeabakker@coldtrick.com>
2015-04-01 15:04:41 +0200

    no longer automaticly set status to sending when clicking 'send now button'

--- actions/send.php
+++ actions/send.php
@@ -7,40 +7,35 @@ $guid = (int) get_input("guid");
 $status_notification = get_input("status_notification");
 $show_in_archive = (int) get_input("show_in_archive");
 
-$forward_url = REFERER;
-
-if (!empty($guid)) {
-	$entity = get_entity($guid);
-	
-	if (!empty($entity) && $entity->canEdit()) {
-		if (elgg_instanceof($entity, "object", Newsletter::SUBTYPE)) {
-			$entity->scheduled = mktime(date("G"), 0, 1, date("n"), date("j"), date("Y"));
-			$entity->status = "sending";
-			$entity->show_in_archive = $show_in_archive;
-			
-			// status notification email address
-			if (!empty($status_notification)) {
-				$entity->status_notification = $status_notification;
-			} else {
-				unset($entity->status_notification);
-			}
-			
-			newsletter_start_commandline_sending($entity);
-			
-			// sleep to make sure the log page has content
-			sleep(2);
-			
-			$forward_url = "newsletter/log/" . $entity->getGUID();
-			
-			system_message(elgg_echo("newsletter:action:send:success"));
-		} else {
-			register_error(elgg_echo("ClassException:ClassnameNotClass", array($guid, elgg_echo("item:object:" . Newsletter::SUBTYPE))));
-		}
-	} else {
-		register_error(elgg_echo("InvalidParameterException:NoEntityFound"));
-	}
-} else {
+if (empty($guid)) {
 	register_error(elgg_echo("InvalidParameterException:MissingParameter"));
+	forward(REFERER);
 }
 
-forward($forward_url);
+$entity = get_entity($guid);
+if (empty($entity) || !$entity->canEdit()) {
+	register_error(elgg_echo("InvalidParameterException:NoEntityFound"));
+	forward(REFERER);
+}
+
+if (!elgg_instanceof($entity, "object", Newsletter::SUBTYPE)) {
+	register_error(elgg_echo("ClassException:ClassnameNotClass", array($guid, elgg_echo("item:object:" . Newsletter::SUBTYPE))));
+}
+
+$entity->scheduled = mktime(date("G"), 0, 1, date("n"), date("j"), date("Y"));
+$entity->show_in_archive = $show_in_archive;
+
+// status notification email address
+if (!empty($status_notification)) {
+	$entity->status_notification = $status_notification;
+} else {
+	unset($entity->status_notification);
+}
+
+newsletter_start_commandline_sending($entity);
+
+// sleep to make sure the log page has content
+sleep(2);
+
+system_message(elgg_echo("newsletter:action:send:success"));
+forward("newsletter/log/" . $entity->getGUID());
