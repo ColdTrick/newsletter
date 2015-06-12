caf3d12942c26781d3f72002795195f0596e2c87
Jerome Bakker <jeabakker@coldtrick.com>
2015-06-12 10:10:55 +0200

    fixed: tag_tools notifications on new newsletter

--- lib/hooks.php
+++ lib/hooks.php
@@ -612,3 +612,35 @@ function newsletter_widget_url_handler($hook_name, $entity_type, $return_value, 
 	
 	return $result;
 }
+
+/**
+ * Modify the tag_tools type/subtypes for notifications
+ *
+ * @param string $hook_name    the name of the hook
+ * @param string $entity_type  the type of the hook
+ * @param array  $return_value current return value
+ * @param array  $params       supplied params
+ *
+ * @return array|void
+ */
+function newsletter_tag_tools_notifications($hook_name, $entity_type, $return_value, $params) {
+	
+	if (empty($return_value) || !is_array($return_value)) {
+		return;
+	}
+	
+	$object_subtypes = elgg_extract('object', $return_value);
+	if (empty($object_subtypes) || !is_array($object_subtypes)) {
+		return;
+	}
+	
+	$key = array_search(Newsletter::SUBTYPE, $object_subtypes);
+	if ($key === false) {
+		return;
+	}
+	
+	unset($object_subtypes[$key]);
+	$return_value['object'] = array_values($object_subtypes);
+	
+	return $return_value;
+}
--- start.php
+++ start.php
@@ -60,7 +60,9 @@ function newsletter_init() {
 	elgg_register_plugin_hook_handler("usersettings:save", "user", "newsletter_usersettings_save_handler");
 	elgg_register_plugin_hook_handler("register", "user", "newsletter_register_user_handler");
 	elgg_register_plugin_hook_handler("entity:icon:url", "object", "newsletter_icon_hook");
+	
 	elgg_register_plugin_hook_handler("widget_url", "widget_manager", "newsletter_widget_url_handler");
+	elgg_register_plugin_hook_handler("notification_type_subtype", "tag_tools", "newsletter_tag_tools_notifications");
 	
 	// extend public pages
 	elgg_register_plugin_hook_handler('public_pages', 'walled_garden', 'newsletter_public_pages');
