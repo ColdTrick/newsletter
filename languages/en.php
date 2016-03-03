30ca8905dcdb329e8396795e677dd42fa371bbb6
Jeroen Dalsem <jdalsem@coldtrick.com>
2016-03-03 18:01:46 +0100

    fixed: added missing CLI secret error message

--- languages/en.php
+++ languages/en.php
@@ -15,6 +15,8 @@ return array(
 	'newsletter:status:scheduled' => "Scheduled",
 	'newsletter:status:sent' => "Sent",
 	
+	'newsletter:cli:error:secret' => "Invalid secret for use in CLI",
+	
 	// CSV upload
 	'newsletter:csv:no_email' => "No e-mail column could be found in the first 2 rows of the CSV-file",
 	'newsletter:csv:added' => "Added %s e-mail addresses from the CSV-file",
