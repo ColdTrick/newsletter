a38d809d4395da44d62c8e35851805cf68a1f933
Jerome Bakker <jeabakker@coldtrick.com>
2017-01-19 14:58:59 +0100

    fixed: subtype class registration

--- activate.php
+++ activate.php
@@ -4,13 +4,17 @@
  *
  */
 
-// update the newsletter subtype to it's own class (when the plugin was actived before)
-if (!update_subtype('object', Newsletter::SUBTYPE, 'Newsletter')) {
+if (get_subtype_id('object', Newsletter::SUBTYPE)) {
+	// update the newsletter subtype to it's own class (when the plugin was actived before)
+	update_subtype('object', Newsletter::SUBTYPE, 'Newsletter');
+} else {
 	// first time the plugin was activated
 	add_subtype('object', Newsletter::SUBTYPE, 'Newsletter');
 }
 
-if (!update_subtype('object', NewsletterSubscription::SUBTYPE, 'NewsletterSubscription')) {
+if (get_subtype_id('object', NewsletterSubscription::SUBTYPE)) {
+	update_subtype('object', NewsletterSubscription::SUBTYPE, 'NewsletterSubscription');
+} else {
 	// first time the plugin was activated
 	add_subtype('object', NewsletterSubscription::SUBTYPE, 'NewsletterSubscription');
 }
