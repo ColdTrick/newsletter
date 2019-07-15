28b4d540a18bdffd7f122221e1b6fff3bbc8746e
Jerome Bakker <jeabakker@coldtrick.com>
2019-07-15 10:27:47 +0200

    fixed: unable to unsubscribre from all newsletters

--- actions/newsletter/unsubscribe.php
+++ actions/newsletter/unsubscribe.php
@@ -15,46 +15,64 @@ if (empty($entity_guid) || empty($recipient)) {
 }
 
 $entity = get_entity($entity_guid);
-if (!empty($entity) && (empty($code) || newsletter_validate_unsubscribe_code($entity, $recipient, $code))) {
-	// what to unsubscribe
-	if (!empty($guid)) {
-		// unsubscribe one newsletter
-		if (is_numeric($recipient) && ($user = get_user($recipient))) {
-			if (newsletter_unsubscribe_user($user, $entity)) {
-				return elgg_ok_response('', elgg_echo('newsletter:action:unsubscribe:success:entity', [$entity->name]));
-			} else {
-				return elgg_error_response(elgg_echo('newsletter:action:unsubscribe:error:entity', [$entity->name]));
-			}
-		} elseif (newsletter_is_email_address($recipient)) {
-			if (newsletter_unsubscribe_email($recipient, $entity)) {
-				return elgg_ok_response('', elgg_echo('newsletter:action:unsubscribe:success:entity', [$entity->name]));
-			} else {
-				return elgg_error_response(elgg_echo('newsletter:action:unsubscribe:error:entity', [$entity->name]));
-			}
-		} else {
-			$recipient_error = true;
-			return elgg_error_response(elgg_echo('newsletter:action:unsubscribe:error:recipient', [$recipient]));
-		}
-	}
-	
-	// unsubscribe from all
-	if (!empty($all)) {
-		if (is_numeric($recipient) && ($user = get_user($recipient))) {
-			if (newsletter_unsubscribe_all_user($user)) {
-				return elgg_ok_response('', elgg_echo('newsletter:action:unsubscribe:success:all'));
-			} else {
-				return elgg_error_response(elgg_echo('newsletter:action:unsubscribe:error:all'));
-			}
-		} elseif (newsletter_is_email_address($recipient)) {
-			if (newsletter_unsubscribe_all_email($recipient)) {
-				return elgg_ok_response('', elgg_echo('newsletter:action:unsubscribe:success:all'));
-			} else {
-				return elgg_error_response(elgg_echo('newsletter:action:unsubscribe:error:all'));
-			}
-		} else {
-			return elgg_error_response(elgg_echo('newsletter:action:unsubscribe:error:recipient', [$recipient]));
-		}
-	}
-} else {
+if (empty($entity) || !(empty($code) || newsletter_validate_unsubscribe_code($entity, $recipient, $code))) {
 	return elgg_error_response(elgg_echo('newsletter:unsubscribe:error:code'));
 }
+
+$user = false;
+$error = false;
+
+// what to unsubscribe
+if (!empty($guid)) {
+	// unsubscribe one newsletter
+	if (is_numeric($recipient) && ($user = get_user($recipient))) {
+		if (newsletter_unsubscribe_user($user, $entity)) {
+			system_message(elgg_echo('newsletter:action:unsubscribe:success:entity', [$entity->getDisplayName()]));
+		} else {
+			$error = true;
+			register_error(elgg_echo('newsletter:action:unsubscribe:error:entity', [$entity->getDisplayName()]));
+		}
+	} elseif (newsletter_is_email_address($recipient)) {
+		if (newsletter_unsubscribe_email($recipient, $entity)) {
+			system_message(elgg_echo('newsletter:action:unsubscribe:success:entity', [$entity->getDisplayName()]));
+		} else {
+			$error = true;
+			register_error(elgg_echo('newsletter:action:unsubscribe:error:entity', [$entity->getDisplayName()]));
+		}
+	} else {
+		$error = true;
+		register_error(elgg_echo('newsletter:action:unsubscribe:error:recipient', [$recipient]));
+	}
+}
+
+// unsubscribe from all
+if (!empty($all)) {
+	if (is_numeric($recipient) && ($user = get_user($recipient))) {
+		if (newsletter_unsubscribe_all_user($user)) {
+			system_message(elgg_echo('newsletter:action:unsubscribe:success:all'));
+		} else {
+			$error = true;
+			register_error(elgg_echo('newsletter:action:unsubscribe:error:all'));
+		}
+	} elseif (newsletter_is_email_address($recipient)) {
+		if (newsletter_unsubscribe_all_email($recipient)) {
+			system_message(elgg_echo('newsletter:action:unsubscribe:success:all'));
+		} else {
+			$error = true;
+			register_error(elgg_echo('newsletter:action:unsubscribe:error:all'));
+		}
+	} else {
+		$error = true;
+		register_error(elgg_echo('newsletter:action:unsubscribe:error:recipient', [$recipient]));
+	}
+}
+
+if ($error) {
+	return elgg_error_response();
+} elseif ($user instanceof ElggUser) {
+	return elgg_ok_response('', '', elgg_generate_url('collection:object:newsletter:subscriptions', [
+		'username' => $user->username,
+	]));
+}
+
+return elgg_ok_response('', '', '');
