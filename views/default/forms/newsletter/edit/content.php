ff0f21249bad6869fe4c793705666d87f2654409
Jeroen Dalsem <jdalsem@coldtrick.com>
2020-01-21 11:31:57 +0100

    chore: added view var to control header upload in content

--- views/default/forms/newsletter/edit/content.php
+++ views/default/forms/newsletter/edit/content.php
@@ -21,13 +21,15 @@ echo elgg_view_field([
 	'value' => elgg_extract('content', $vars),
 ]);
 
-echo elgg_view('output/text', ['value' => elgg_echo('newsletter:edit:content:header')]);
-
-echo elgg_view('entity/edit/icon', [
-	'entity' => $entity,
-	'entity_type' => 'object',
-	'entity_subtype' => 'newsletter',
-]);
+if (elgg_extract('upload_header', $vars, true)) {
+	echo elgg_view('output/text', ['value' => elgg_echo('newsletter:edit:content:header')]);
+	
+	echo elgg_view('entity/edit/icon', [
+		'entity' => $entity,
+		'entity_type' => 'object',
+		'entity_subtype' => 'newsletter',
+	]);
+}
 
 // footer
 $footer = elgg_view_field([
